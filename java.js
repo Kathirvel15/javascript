73bf5f154a21764ac819bc5fc0e5ffedc9bd21fc
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-04-17 21:52:40 +0530

    javascript

---
+++ java.js
@@ -0,0 +1,31 @@
+console.log("Thala");
+var a=['apple','career','labs']
+console.log(a[1]);
+var demo=document.getElementById("demo").innerHTML;
+console.log(demo);
+
+var abcd={fruit:"apple",color:"red"};
+console.log(abcd);
+
+
+//clear console
+console.log("I want to clear the console")
+function clickk() {
+    console.clear()
+  }
+
+  //for loop
+  for(let i=0;i<5;i++)
+  {
+      console.count()
+  }
+  
+  //trace
+  function click2()
+  {
+      myfunction();
+  }
+  function myfunction()
+  {
+      console.trace();
+  }
